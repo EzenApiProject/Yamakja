a60bf379aca18b734ee4b8fc28a9cc26d3772e0e
Kim Seonghun <pl1633@naver.com>
2023-09-15 13:11:17 +0900

    금요일

--- shop/src/main/java/com/yamakja/shop/mapper/OrderMapper.java
+++ shop/src/main/java/com/yamakja/shop/mapper/OrderMapper.java
@@ -4,11 +4,10 @@ import com.yamakja.shop.domain.Cart;
 import com.yamakja.shop.domain.OrderList;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Mapper
 public interface OrderMapper {
-    void saveOrder(@Param("memberId") String memberId,@Param("carts") List<Cart> carts) throws Exception;
+    void saveOrder(String memberId,List<Cart> carts) throws Exception;
     List<OrderList> getOrderList(String memberId) throws Exception;
 }
