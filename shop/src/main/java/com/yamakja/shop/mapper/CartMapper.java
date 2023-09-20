3b24c9728b9f09fb2e67b9bc0f801a894e2e60d6
Kim Saehan <user@gmail.com>
2023-09-20 18:05:21 +0900

    no message

--- shop/src/main/java/com/yamakja/shop/mapper/CartMapper.java
+++ shop/src/main/java/com/yamakja/shop/mapper/CartMapper.java
@@ -5,7 +5,6 @@ import com.yamakja.shop.domain.CartItem;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import java.math.BigInteger;
 import java.util.List;
 
 @Mapper
@@ -15,14 +14,10 @@ public interface CartMapper {
     Cart getItemsByCartItemId(int itemId,String memberId);
     Integer hasItem(String memberId, int itemId);
     void updateCartItem(CartItem cartItem);
-<<<<<<< HEAD
     Integer totalPrice(String memberId);
-=======
     void updateCartItemByCart(Cart cart,String memberId);
-    BigInteger totalPrice(String memberId);
     Integer getQuantityById(int itemId,String memberId);
     void deleteCart(int cartId, String memberId);
 
     Boolean hasCart(String memberId,Integer itemId);
->>>>>>> cbb5d2f643b54ae0551b96932a48e7e57cf74fe6
 }
